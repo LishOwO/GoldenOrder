a8578cc1e163545c949eb356e6a6f499a3cadf57
MartinHup <mart.06@outlook.fr>
2025-01-23 09:28:08 +0100

    persosoldat

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ class Game:
         self.background_image = pygame.transform.scale(self.background_image, self.BACKGROUND_TILESET_SIZE)
 
         # Chargement du joueur
-        self.player_image = self.load_and_resize_image('src/images/sprite/player/MecAlakippa',
+        self.player_image = self.load_and_resize_image('src/images/sprite/player/Soldier.perso',
                                                        self.PLAYER_SIZE_MULTIPLIER)
 
         # Chargement du zombie
