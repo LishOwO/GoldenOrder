9362c2a3f26e3adf00cf9aa7b41757d6f3a884a5
Vincent <61601267+LishOwO@users.noreply.github.com>
2025-02-06 08:44:06 +0100

    ajout var sons

--- main.py
+++ main.py
@@ -52,6 +52,11 @@ class Game:
                                      pygame.mixer.Sound("src/son/ZombieDamage3.mp3"),
                                      pygame.mixer.Sound("src/son/ZombieDamage4.mp3"), ]
 
+        # Son Powerups
+        self.son_bombe = pygame.mixer.Sound("src/son/BombSound.mp3")
+        self.son_soin = pygame.mixer.Sound("src/son/HealthBoost.mp3")
+
+
         # Var BULLET
         self.BULLET_VELOCITY = 10
         self.BULLET_SIZE = 2
@@ -355,11 +360,11 @@ class Game:
         if choice == "bombe":
             print("Effet: Bombe")
             self.player_effect_bomb()
-            self.son_bombe = pygame.mixer.Sound("src/son/BombSound.mp3")
+            
         elif choice == "soin":
             print("Effet: Soin")
             self.player_effect_heal()
-            self.son_soin = pygame.mixer.Sound("src/son/HealthBoost.mp3")
+
         elif choice == "invincibilite":
             print("Effet: Invincibilité")
             self.player_effect_invincibility()
